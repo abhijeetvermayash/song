dbb09a9597568a062e9917d6ed749097f0e6c520
abhijeet <abhijeetvermayash@gmail.com>
2020-06-19 17:02:59 +0530

    Updates

---
+++ precache-manifest.2d79ed869190f3133c784f4c87ad35b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "676cc935f5449c55d377452651cb412a",
+    "url": "/song/index.html"
+  },
+  {
+    "revision": "a6b3433466af62090a7d",
+    "url": "/song/static/js/2.2fa9a0c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/song/static/js/2.2fa9a0c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ad0dbaa051c06f6ef29",
+    "url": "/song/static/js/main.937e7c46.chunk.js"
+  },
+  {
+    "revision": "827293261eecc0e7569f",
+    "url": "/song/static/js/runtime-main.cb05810f.js"
+  }
+]);
